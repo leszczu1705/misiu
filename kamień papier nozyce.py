e867df185236da22b9e83836f609dcf096629801
leszczu1705 <mikolaj.mis2010@gmail.com>
2025-03-26 13:19:19 +0100

    htehgre

---
+++ kamień papier nozyce.py
@@ -0,0 +1,15 @@
+import random
+
+opcje=("kamien","papier","nozyce")
+a=random.choice(opcje)
+b=input("Podaj swoją opcję: ")
+if a==b:
+    print("Remis")
+elif a=="kamien" and b=="papier":
+    print("Wygrałeś")
+elif a=="nozyce" and b=="papier":
+    print("Przegrałeś")
+elif a=="kamien" and b=="nozyce":
+    print("Przegrałeś")
+elif a=="papier" and b=="nozyce":
+    print("Wygrałeś")
