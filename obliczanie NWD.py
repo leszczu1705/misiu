750cf800bc577c872c5c5938d739f39f68531c00
leszczu1705 <mikolaj.mis2010@gmail.com>
2025-04-09 11:52:08 +0200

    0000

--- obliczanie NWD.py
+++ obliczanie NWD.py
@@ -1,8 +1,15 @@
 a=int(input("Podaj pierwszą liczbę całkowitą większą od zera: "))
 b=int(input("Podaj drugą liczbę całkowitą większą od zera: "))
-while a!=b:
-    if a>b:
-        a-=b
-    else:
-        b-=a
-print(a)
+if a>0 and b>0:
+    while a!=b:
+        if a>b:
+            a-=b
+        else:
+            b-=a
+        print(a)
+elif a<=0 and b<=0:
+    print("Obie liczby nie są większe od zera. Podaj liczby ponownie")
+elif a<=0:
+    print("Pierwsza liczba jest mniejsza od zera. Podaj liczby ponownie")
+else:
+    print("Druga liczba jest mniejsza od zera. Podaj liczby ponownie")
