1d400e0b37de4e6c8177741bf05c7b2adac6b81e
leszczu1705 <mikolaj.mis2010@gmail.com>
2025-04-23 12:11:11 +0200

    =-0987654321

--- obliczanie NWW.py
+++ obliczanie NWW.py
@@ -2,10 +2,17 @@ a=int(input("Podaj pierwszą liczbę całkowitą większą od zera: "))
 b=int(input("Podaj drugą liczbę całkowitą większą od zera: "))
 c=a
 d=b
-while a!=b:
-    if a>b:
-        a-=b
-    else:
-        b-=a
-e=c*d/a
-print(e)
+if a>0 and b>0:
+    while a!=b:
+        if a>b:
+            a-=b
+        else:
+            b-=a
+    e=c*d/a
+    print(e)
+elif a<=0 and b<=0:
+    print("Obie liczby nie są większe od zera. Podaj liczby ponownie")
+elif a<=0:
+    print("Pierwsza liczba jest mniejsza od zera. Podaj liczby ponownie")
+else:
+    print("Druga liczba jest mniejsza od zera. Podaj liczby ponownie")
